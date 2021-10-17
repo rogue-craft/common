d0ef0cacb3a6ccf3a25d7d4693ca59e3463d5b67
Isty001 <isty001@gmail.com>
2021-10-17 16:09:40 +0200

    hitbox poistion

--- lib/rogue-craft-common/interpolation/position.rb
+++ lib/rogue-craft-common/interpolation/position.rb
@@ -3,11 +3,26 @@ module Interpolation
   def self.position(x, y, speed, direction, since)
     distance = (Time.now_ms - since) * speed
 
-    y = (y + distance).floor unless 0 == direction & Interpolation::Direction::NORTH
-    y = (y - distance).ceil unless 0 == direction & Interpolation::Direction::SOUTH
-    x = (x + distance).floor unless 0 == direction & Interpolation::Direction::EAST
-    x = (x - distance).ceil unless 0 == direction & Interpolation::Direction::WEST
+    unless 0 == direction & Interpolation::Direction::NORTH
+      y = (y + distance)
+      hitbox_y = y.floor
+    end
 
-    [x, y]
+    unless 0 == direction & Interpolation::Direction::SOUTH
+      y = (y - distance)
+      hitbox_y = y.ceil
+    end
+
+    unless 0 == direction & Interpolation::Direction::EAST
+      x = (x + distance)
+      hitbox_x = x.floor
+    end
+
+    unless 0 == direction & Interpolation::Direction::WEST
+      x = (x - distance)
+      hitbox_x = x.ceil
+    end
+
+    [x, y, hitbox_x, hitbox_y]
   end
 end
